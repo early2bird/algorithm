bbfc1c8c2b34e35eff52db634bb3a26413d3ac78
nestops <qabcdef9@163.com>
2021-09-25 21:03:36 +0800

    修改base

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,8 @@
 module.exports = {
     title: 'Hello VuePress',
     description: 'Just playing around',
+    dest: './dist',
+    base: '/early2bird/',
     markdown: {
         lineNumbers: true
     }
